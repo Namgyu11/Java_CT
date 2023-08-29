24f0fd27d919640f0c5b0b68b8844c9a6d205da4
Namgyu11 <hagaji3@naver.com>
2023-08-29 13:55:32 +0900

    ct

---
+++ src/codingTest/preCT/ct09/ct09_02/Solution.java
@@ -0,0 +1,24 @@
+package codingTest.preCT.ct09.ct09_02;
+
+public class Solution {
+    public static int solution(int[] nums, int n) {
+        int answer = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] == n){
+                if(answer > i || answer == -1){
+                    answer = i;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 3, 2};
+        int n = 3;
+        System.out.println(solution(nums,n));
+
+    }
+}
