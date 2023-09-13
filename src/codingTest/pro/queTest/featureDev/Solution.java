103264205c82f6d1e3046893a4e973722daa281d
Namgyu11 <hagaji3@naver.com>
2023-09-13 23:33:03 +0900

    pro 배열 - 스택/큐 -  기능개발

---
+++ src/codingTest/pro/queTest/featureDev/Solution.java
@@ -0,0 +1,19 @@
+package codingTest.pro.queTest.featureDev;
+
+import java.util.ArrayDeque;
+
+/*
+프로그래머스 - 스택/큐 - "기능개발"
+ */
+public class Solution {
+
+    public static int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        ArrayDeque arrayDeque = new ArrayDeque<>();
+        return answer;
+    }
+    public static void main(String[] args) {
+
+    }
+
+}
