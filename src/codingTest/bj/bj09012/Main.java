c3e2287d8de7097a17abda790e0746e5c1da56d6
Namgyu11 <hagaji3@naver.com>
2023-09-18 23:40:24 +0900

    bj09012 - 스택 괄호

--- src/codingTest/bj/bj09012/Main.java
+++ src/codingTest/bj/bj09012/Main.java
@@ -5,26 +5,26 @@ import java.util.Stack;
 
 public class Main {
     public static void solution(String str) {
-        Stack stack = new Stack<>();
-        boolean flag = true;
-        for (String s : str.split("")) {
-            if (s.equals("(")) {
-                stack.push(s);
-            } else {
-                if (stack.isEmpty()) {
-                    flag = false;
-                    break;
-                } else {
-                    stack.pop();
-                }
-            }
-        }
-        if (flag && stack.isEmpty()) {
-            System.out.println("YES");
-        } else {
-            System.out.println("NO");
+      Stack<String> stack = new Stack<>();
+      boolean flag = true;
 
-        }
+      for(String s : str.split("")){
+          if(s.equals("(")){
+              stack.push(s);
+          }else {
+              if(stack.isEmpty()){
+                  flag = false;
+                  break;
+              }else {
+                  stack.pop();
+              }
+          }
+      }
+      if(flag && stack.isEmpty()){
+          System.out.println("YES");
+      }else {
+          System.out.println("NO");
+      }
     }
 
     public static void main(String[] args) {
