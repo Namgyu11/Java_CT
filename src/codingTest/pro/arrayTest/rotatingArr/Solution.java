eb0739b6550f5a61248f227f58fec9393cfa9b23
Namgyu11 <hagaji3@naver.com>
2023-09-26 19:53:31 +0900

    pro- 한 번만 등장한 문자

---
+++ src/codingTest/pro/arrayTest/rotatingArr/Solution.java
@@ -0,0 +1,51 @@
+package codingTest.pro.arrayTest.rotatingArr;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
+
+public class Solution {
+    public static int[] solution(int[] numbers, String direction) {
+        LinkedList<Integer> list = Arrays.stream(numbers).boxed().collect(Collectors.toCollection(LinkedList::new));
+
+        if(direction.equals("right")){
+            list.offerFirst(list.pollLast());
+        }else {
+            list.offerLast(list.pollFirst());
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
+        /*
+        int[] answer = new int[numbers.length];
+        int temp1 = numbers[numbers.length - 1];
+        int temp2 = numbers[0];
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (direction.equals("right")) {
+                if (i == numbers.length - 1) {
+                    answer[0] = temp1;
+                } else {
+                    answer[i + 1] = numbers[i];
+                }
+            } else {
+                if (i == 0) {
+                } else {
+                    answer[i - 1] = numbers[i];
+                    if (i == numbers.length - 1) {
+                        answer[i] = temp2;
+                    }
+                }
+            }
+        }
+        return answer;
+
+         */
+    }
+
+    public static void main(String[] args) {
+
+        int[] num = {1,2,3};
+        String d = "right";
+        System.out.println(Arrays.toString(solution(num, d)));
+    }
+}
