0f0c9663e874060a820f08f10f7aa52a8b418b28
Namgyu11 <hagaji3@naver.com>
2023-10-07 20:09:17 +0900

    preCT_03_01

---
+++ src/codingTest/preCT_03/preCT_03_01/ct04/Solution.java
@@ -0,0 +1,33 @@
+package codingTest.preCT_03.preCT_03_01.ct04;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
+//23-10-06(풀이) - 다시 풀기
+public class Solution {
+    public static String solution(String s) {
+        int count = 0;
+        boolean inOneGroup = false;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                if (!inOneGroup && count > 0 && count < 3) return "NO";
+                inOneGroup = true;
+                count++;
+            } else {
+                if (inOneGroup && count > 3) return "NO";
+                inOneGroup = false;
+                count = 0;
+            }
+        }
+
+        return "YES";
+    }
+
+    public static void main(String[] args) {
+        String s = "111000010100";
+        String a = "001001000100";
+        System.out.println(solution(a));
+    }
+}
