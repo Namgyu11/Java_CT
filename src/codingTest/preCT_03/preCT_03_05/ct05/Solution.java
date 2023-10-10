08fda9c0c49936a5f7996f317614fe8a799e82a5
Namgyu11 <hagaji3@naver.com>
2023-10-10 19:25:24 +0900

    preCT_03_05

---
+++ src/codingTest/preCT_03/preCT_03_05/ct05/Solution.java
@@ -0,0 +1,42 @@
+package codingTest.preCT_03.preCT_03_05.ct05;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+
+public class Solution {
+    public static int[] solution(int[] arr) {
+        int[] coins = {500, 100, 50, 10};
+        int lastBill = -1;
+        int totalMoney = 0;
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int i = arr.length - 1; i >=0; i--) {
+            if (arr[i] >= 1000) {
+                lastBill = arr[i];
+            }
+            totalMoney += arr[i];
+        }
+
+        if (lastBill != -1) {
+            totalMoney -= lastBill;
+            result.add(lastBill);
+        }
+
+        for (int coin : coins) {
+            while(totalMoney >= coin){
+                result.add(coin);
+                totalMoney -= coin;
+            }
+        }
+
+        return result.stream().mapToInt(i->i).toArray();
+    }
+
+    public static void main(String[] args) {
+        int[] arr ={1000,1000,100};
+        System.out.println(solution(arr));
+    }
+}
