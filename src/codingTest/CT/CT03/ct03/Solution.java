c0e469ed6632465f71ac105f2870a753c05d0548
Namgyu11 <hagaji3@naver.com>
2023-10-31 23:23:40 +0900

    CT03_04

--- src/codingTest/CT/CT03/ct03/Solution.java
+++ src/codingTest/CT/CT03/ct03/Solution.java
@@ -1,5 +1,28 @@
 package codingTest.CT.CT03.ct03;
 
 //23-10-27(풀이) -- 다시 풀기
-public class Solution {
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public int solution(int delay, int N) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
+        int count = 1;
+        for (int i = 1; i <= N; i++) {
+            Queue<Integer> newQueue = new LinkedList<>();
+            while (!queue.isEmpty()) {
+                int d = queue.remove();
+                if (d == 0) {
+                    newQueue.add(0);
+                    newQueue.add(delay);
+                    count += 2;
+                } else {
+                    newQueue.add(d - 1);
+                }
+            }
+            queue = newQueue;
+        }
+        return count;
+    }
 }
