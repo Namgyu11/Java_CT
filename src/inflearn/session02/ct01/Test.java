ab2a5aea1714c492a0f15606ffa873d242b20490
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2024-09-15 23:53:22 +0900

    week05_test

---
+++ src/inflearn/session02/ct01/Test.java
@@ -0,0 +1,51 @@
+package inflearn.session02.ct01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Test {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        String table = br.readLine();
+
+        List<Integer> people = new ArrayList<>();
+        List<Integer> burgers = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            if (table.charAt(i) == 'P') {
+                people.add(i);
+            } else if (table.charAt(i) == 'H') {
+                burgers.add(i);
+            }
+        }
+
+        int maxPeople = 0;
+        int pIndex = 0, bIndex = 0;
+
+        while (pIndex < people.size() && bIndex < burgers.size()) {
+            int person = people.get(pIndex);
+            int burger = burgers.get(bIndex);
+
+            if (Math.abs(person - burger) <= K) {
+                maxPeople++;
+                pIndex++;
+                bIndex++;
+            } else if (person < burger) {
+                pIndex++;
+            } else {
+                bIndex++;
+            }
+        }
+
+        System.out.println(maxPeople);
+    }
+}
