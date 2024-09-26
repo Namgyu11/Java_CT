ae978c0f7fc54f9ab08bdd41d00b98bbf9c2c3c9
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2024-09-26 22:42:51 +0900

    inflearn_session02

---
+++ src/ktb_ct/week06/ct03/Main.java
@@ -0,0 +1,14 @@
+package ktb_ct.week06.ct03;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+    }
+
+}
