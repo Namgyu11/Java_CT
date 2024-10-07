7cc7c9a684e6cdf1cf5f792b319192aac96bcd3a
hanamgyu <hagaji3@naver.com>
2024-10-07 16:17:40 +0900

    inflearn_session02

---
+++ src/inflearn/session02/ct11/Main.java
@@ -0,0 +1,21 @@
+package inflearn.session02.ct11;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        StringBuilder compressed = new StringBuilder();
+
+        int count = 1;
+        for (int i = 0; i < ; i++) {
+
+        }
+    }
+
+}
