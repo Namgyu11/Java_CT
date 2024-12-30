bb1e3ed4d298da11545d9989ac499bf063e55330
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2024-12-30 21:03:06 +0900

    inflearn_session03_05

--- src/inflearn/session03/ct05/Main.java
+++ src/inflearn/session03/ct05/Main.java
@@ -1,20 +1,56 @@
 package inflearn.session03.ct05;
+
 import java.util.*;
 import java.io.*;
-import java.util.stream.*;
+import java.util.stream.IntStream;
 
 public class Main {
 
+    //다시 풀기
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
 
-        int cnt = 0;
+        //boolean[] isPrime = new boolean[n + 1];
 
-        for(int i = 1; i <= n; i++){
+//        Arrays.fill(isPrime, true);
+//
+//        isPrime[0] = false;
+//        isPrime[1] = false;
+//
+//        for (int i = 0; i * i <= n; i++) {
+//            if (isPrime[i]) {
+//                for (int j = i * i; j <= n; j += i){
+//                    isPrime[j] = false;
+//                }
+//            }
+//        }
+//
+//        int count = 0;
+//        for(int i = 2; i <= n; i++){
+//            if(isPrime[i]){
+//                count++;
+//            }
+//        }
 
-        }
+        long count = IntStream.rangeClosed(2, n)
+                .filter(Main::isPrime)
+                .count();
+        System.out.println(count);
     }
 
+
+    private static boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        }
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
--- src/inflearn/session03/ct06/Main.java
+++ src/inflearn/session03/ct06/Main.java
@@ -1,13 +1,13 @@
-package inflearn.session03.ct06;
-
-import java.util.*;
-import java.io.*;
-import java.util.stream.*;
-
-public class Main {
-
-    public static void main(String[] args) throws  {
-
-    }
-
-}
+//package inflearn.session03.ct06;
+//
+//import java.util.*;
+//import java.io.*;
+//import java.util.stream.*;
+//
+//public class Main {
+//
+//    public static void main(String[] args) throws  {
+//
+//    }
+//
+//}
