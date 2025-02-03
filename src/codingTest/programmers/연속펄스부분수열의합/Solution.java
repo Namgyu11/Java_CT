871de111d9efdf4f3d6a455e764bca953513749f
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-02-03 23:30:40 +0900

    pro_연속 펄스 부분 수열의 합

---
+++ src/codingTest/programmers/연속펄스부분수열의합/Solution.java
@@ -0,0 +1,27 @@
+package codingTest.programmers.연속펄스부분수열의합;
+
+
+class Solution {
+    public long solution(int[] sequence) {
+        int N = sequence.length;
+        long[] pulse1 = new long[N];
+        long[] pulse2 = new long[N];
+
+        for (int i = 0; i < N; i++) {
+            pulse1[i] = sequence[i] * (i % 2 == 0 ? 1 : -1);
+            pulse2[i] = sequence[i] * (i % 2 == 0 ? -1 : 1);
+        }
+
+        return Math.max(maxSubarraySum(pulse1), maxSubarraySum(pulse2));
+    }
+
+    private static long maxSubarraySum(long[] arr) {
+        long maxSum = arr[0], currentSum = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        return maxSum;
+    }
+}
