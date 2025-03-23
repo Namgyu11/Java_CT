a34864de3477a01a61437f9d8b2447759679e6e5
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-03-23 22:41:05 +0900

    pro_주차 요금 계산

--- src/codingTest/programmers/lv2/주차요금계산/Solution.java
+++ src/codingTest/programmers/lv2/주차요금계산/Solution.java
@@ -7,43 +7,41 @@ class Solution {
         Map<String, Integer> totalTime = new HashMap<>();
         Map<String, Integer> inTime = new HashMap<>();
 
-        for(String record: records) {
+        for(String record : records) {
             String[] parts = record.split(" ");
             String timeStr = parts[0];
             String car = parts[1];
             String status = parts[2];
-            String[] timeParts = timeStr.split(":");
-            int time = Integer.parseInt(timeParts[0]) * 60 + Integer.parseInt(timeParts[1]);
+            int hour = Integer.parseInt(timeStr.substring(0,2));
+            int minute = Integer.parseInt(timeStr.substring(3));
+            int time = hour * 60 + minute;
 
             if(status.equals("IN")) {
                 inTime.put(car, time);
                 totalTime.putIfAbsent(car, 0);
             } else {
-                int start = inTime.get(car);
-                int diff = time - start;
-                totalTime.put(car, totalTime.get(car) + diff);
+                int in = inTime.get(car);
+                totalTime.put(car, totalTime.get(car) + time - in);
                 inTime.remove(car);
             }
         }
 
         for(String car : inTime.keySet()) {
-            int start = inTime.get(car);
-            int diff = (23 * 60 + 59) - start;
-            totalTime.put(car, totalTime.get(car) + diff);
+            int in = inTime.get(car);
+            totalTime.put(car, totalTime.get(car) + ((23 * 60 + 59) - in));
         }
+
         List<String> cars = new ArrayList<>(totalTime.keySet());
         Collections.sort(cars);
+
         int[] answer = new int[cars.size()];
         int baseTime = fees[0], baseFee = fees[1], unitTime = fees[2], unitFee = fees[3];
-        int idx = 0;
 
-        for(String car : cars) {
-            int time = totalTime.get(car);
+        for(int i = 0; i < cars.size(); i++){
+            int time = totalTime.get(cars.get(i));
             int fee = baseFee;
-            if (time > baseTime) {
-                fee += Math.ceil((time - baseTime) / (double) unitTime) * unitFee;
-            }
-            answer[idx++] = fee;
+            if(time > baseTime) fee += (int)Math.ceil((time - baseTime) / (double)unitTime) * unitFee;
+            answer[i] = fee;
         }
         return answer;
     }
