9e3d5a3d2681b216d953543d787fffe344431e4c
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-04-05 20:03:48 +0900

    bj02798_블랙잭

---
+++ src/codingTest/bj/bj02798/Main.java
@@ -0,0 +1,29 @@
+package codingTest.bj.bj02798;
+
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] cards = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            cards[i] = Integer.parseInt(st.nextToken());
+        }
+        int best = 0;
+        for(int i = 0; i < N - 2; i++){
+            for(int j = i + 1; j < N - 1; j++){
+                for(int k = j + 1; k < N; k++){
+                    int sum = cards[i] + cards[j] + cards[k];
+                    if(sum <= M && sum > best) {
+                        best = sum;
+                    }
+                }
+            }
+        }
+        System.out.println(best);
+    }
+}
