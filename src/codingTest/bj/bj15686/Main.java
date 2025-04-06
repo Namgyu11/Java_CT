8c3c732de731fa31f0b165aba654891b2e25f227
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-04-06 17:23:33 +0900

    bj02798_치킨배달

--- src/codingTest/bj/bj15686/Main.java
+++ src/codingTest/bj/bj15686/Main.java
@@ -1,84 +1,67 @@
 package codingTest.bj.bj15686;
 
 // 백준 15686, 치킨 배달
-// 2025.02.02
 import java.io.*;
 import java.util.*;
 
-class Pair {
-    public int y, x;
-
-    public Pair(int y, int x) {
-        this.y = y;
-        this.x = x;
-    }
-}
-
 public class Main {
-    static int n;
-    static int m;
-    static int minSumDist = Integer.MAX_VALUE;
+    static int N, M;
+    static int[][] city;
+    static ArrayList<Point> houses = new ArrayList<>();
+    static ArrayList<Point> chickens = new ArrayList<>();
+    static int ans = Integer.MAX_VALUE;
 
-    static List<Pair> homeList = new ArrayList<>();
-    static List<Pair> chickenList = new ArrayList<>();
-    static boolean[] selected;
-    static final int EMPTY = 0, HOME = 1, CHICKEN = 2;
-
-    static void backtrack(int chickenIdx, int selectCnt) {
-        if (selectCnt == m) {
-            int sumDist = 0;
-
-            for (Pair home : homeList) {
-                int minDist = Integer.MAX_VALUE;
-
-                for (int i = 0; i < chickenList.size(); i++) {
-                    if (selected[i]) {
-                        minDist = Math.min(minDist, calcDist(home, chickenList.get(i)));
-                    }
-                }
-
-                sumDist += minDist;
-            }
-
-            minSumDist = Math.min(minSumDist, sumDist);
-            return;
-        }
-
-        for (int i = chickenIdx; i < chickenList.size(); i++) {
-            selected[i] = true;
-            backtrack(i + 1, selectCnt + 1);
-
-            selected[i] = false;
+    static class Point {
+        int r, c;
+        Point(int r, int c) {
+            this.r = r;
+            this.c = c;
         }
     }
 
-    static int calcDist(Pair home, Pair chicken) {
-        return Math.abs(home.y - chicken.y) + Math.abs(home.x - chicken.x);
-    }
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        for (int i = 0; i < n; i++) {
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        city = new int[N][N];
+        for (int i = 0; i < N; i++){
             st = new StringTokenizer(br.readLine());
-
-            for (int j = 0; j < n; j++) {
-                int input = Integer.parseInt(st.nextToken());
-                if (input == HOME) {
-                    homeList.add(new Pair(i, j));
-                } else if (input == CHICKEN) {
-                    chickenList.add(new Pair(i, j));
+            for (int j = 0; j < N; j++){
+                city[i][j] = Integer.parseInt(st.nextToken());
+                if (city[i][j] == 1) {
+                    houses.add(new Point(i, j));
+                } else if (city[i][j] == 2) {
+                    chickens.add(new Point(i, j));
                 }
             }
         }
+        combination(0, 0, new int[M]);
+        System.out.println(ans);
+    }
 
-        selected = new boolean[chickenList.size()];
-        backtrack(0, 0);
+    static void combination(int start, int cnt, int[] sel) {
+        if (cnt == M) {
+            int sum = calc(sel);
+            ans = Math.min(ans, sum);
+            return;
+        }
+        for (int i = start; i < chickens.size(); i++) {
+            sel[cnt] = i;
+            combination(i + 1, cnt + 1, sel);
+        }
+    }
 
-        System.out.println(minSumDist);
+    static int calc(int[] sel) {
+        int sum = 0;
+        for (Point house : houses) {
+            int dist = Integer.MAX_VALUE;
+            for (int idx : sel) {
+                Point chicken = chickens.get(idx);
+                dist = Math.min(dist, Math.abs(house.r - chicken.r) + Math.abs(house.c - chicken.c));
+            }
+            sum += dist;
+        }
+        return sum;
     }
 }
