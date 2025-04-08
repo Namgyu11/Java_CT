7121ada62008e5edc514ed5fcfc4d27083e872d8
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-04-08 23:08:17 +0900

    bj02638_치즈

---
+++ src/codingTest/preCT_01/bj02830/Main.java
@@ -0,0 +1,31 @@
+package codingTest.preCT_01.bj02830;
+
+import java.util.Scanner;
+
+public class Main {
+//    public static int solution(boolean count) {
+//        Scanner sc = new Scanner(System.in);
+//        int result1 = 0;
+//        int i = 0;
+//        for (int i = 0; i < count; i++) {
+//            int temp = sc.nextInt();
+//            if(i == 0){
+//                result1 = temp;
+//            }else {
+//                result1 ^= temp;
+//            }
+//        }
+//
+//
+//        return result1;
+//    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+//        System.out.println(solution(sc.hasNextInt()));
+
+
+    }
+}
---
+++ src/codingTest/preCT_01/bj09012/Main.java
@@ -0,0 +1,38 @@
+package codingTest.preCT_01.bj09012;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void solution(String str) {
+      Stack<String> stack = new Stack<>();
+      boolean flag = true;
+
+      for(String s : str.split("")){
+          if(s.equals("(")){
+              stack.push(s);
+          }else {
+              if(stack.isEmpty()){
+                  flag = false;
+                  break;
+              }else {
+                  stack.pop();
+              }
+          }
+      }
+      if(flag && stack.isEmpty()){
+          System.out.println("YES");
+      }else {
+          System.out.println("NO");
+      }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for (int i = 0; i < num; i++) {
+            solution(sc.next());
+        }
+
+    }
+}
