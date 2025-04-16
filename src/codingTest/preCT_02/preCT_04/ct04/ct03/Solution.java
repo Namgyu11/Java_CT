e51eac4e8e95e9c4e3c64820d422a9874db69ed5
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-04-16 23:13:36 +0900

    pro_

---
+++ src/codingTest/preCT_02/preCT_04/ct04/ct03/Solution.java
@@ -0,0 +1,17 @@
+package codingTest.preCT_02.preCT_04.ct04.ct03;
+
+//23-09-30 (풀이) - 다시 풀기
+public class Solution {
+    public static int solution(String S1, String S2) {
+        String doubledS1 = S1 + S1;
+        return doubledS1.contains(S2) ? 1 : 0;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "helloworld";
+        String s2 = "worldhello";
+
+
+        System.out.println(solution(s1,s2));
+    }
+}
