b46007ff30865e82a33d8d049f6f89e4fff0efe7
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-04-17 23:03:20 +0900

    pro_

---
+++ src/codingTest/preCT_02/preCT_04/ct02/ct04/Solution.java
@@ -0,0 +1,20 @@
+package codingTest.preCT_02.preCT_04.ct02.ct04;
+
+public class Solution {
+    public static int solution(int[] nums) {
+        int count = 0;
+        for (int i = 0; i <nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if(nums[i] == nums[j]){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,5,6,3,2,6,6};
+        System.out.println(solution(nums));
+    }
+}
