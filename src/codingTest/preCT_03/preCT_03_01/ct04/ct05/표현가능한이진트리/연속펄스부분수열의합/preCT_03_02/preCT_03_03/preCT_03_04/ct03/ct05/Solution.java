ff22e0615488ca462884d4bd6e7f8ce434e9d2b9
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-05-19 22:01:34 +0900

    pro_

---
+++ src/codingTest/preCT_03/preCT_03_01/ct04/ct05/표현가능한이진트리/연속펄스부분수열의합/preCT_03_02/preCT_03_03/preCT_03_04/ct03/ct05/Solution.java
@@ -0,0 +1,22 @@
+package codingTest.preCT_03.preCT_03_01.ct04.ct05.표현가능한이진트리.연속펄스부분수열의합.preCT_03_02.preCT_03_03.preCT_03_04.ct03.ct05;
+
+public class Solution {
+    public boolean solution(int[] bridge, int jumpSize) {
+        boolean answer = true;
+        int cnt = 0;
+        int temp = 0;
+        for (int i = 0; i < bridge.length; i++) {
+            if(bridge[i] == 0){
+                cnt++;
+                temp = Math.max(cnt,temp);
+            }else {
+                cnt = 0;
+            }
+        }
+        if(temp > jumpSize){
+            answer = false;
+        }
+        return answer;
+    }
+
+}
