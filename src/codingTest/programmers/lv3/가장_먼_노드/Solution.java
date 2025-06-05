40662d28cb8c4d42cb4cfe028b131a80abdc0be4
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-06-05 19:16:27 +0900

    pro_가장 먼 노드

---
+++ src/codingTest/programmers/lv3/가장_먼_노드/Solution.java
@@ -0,0 +1,50 @@
+package codingTest.programmers.lv3.가장_먼_노드;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[][] vertex) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int[] edge : vertex) {
+            int u = edge[0], v = edge[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, -1);
+        Queue<Integer> q = new LinkedList<>();
+        
+        dist[1] = 0;
+        q.offer(1);
+        
+        while (!q.isEmpty()) {
+            int u = q.poll();
+            for (int v : adj.get(u)) {
+                if (dist[v] == -1) {
+                    dist[v] = dist[u] + 1;
+                    q.offer(v);
+                }
+            }
+        }
+        
+        int maxDist = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] > maxDist) {
+                maxDist = dist[i];
+            }
+        }
+        
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] == maxDist) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
