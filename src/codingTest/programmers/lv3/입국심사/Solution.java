7ae1d8f96bb3ea5ab39d6a6ae7679fe6053f6a85
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-06-07 23:42:43 +0900

    pro_입국 심사

---
+++ src/codingTest/programmers/lv3/입국심사/Solution.java
@@ -0,0 +1,35 @@
+package codingTest.programmers.lv3.입국심사;
+
+class Solution {
+    public long solution(long n, int[] times) {
+        long low = 1;
+        long high = 0;
+        for (int t : times) {
+            high = Math.max(high, t);
+        }
+        high *= n;  
+        
+        long answer = high;
+        while (low <= high) {
+            long mid = (low + high) >>> 1;
+            if (canProcessAll(n, times, mid)) {
+                answer = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return answer;
+    }
+    
+    private boolean canProcessAll(long n, int[] times, long t) {
+        long count = 0;
+        for (int time : times) {
+            count += t / time;
+            if (count >= n) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
