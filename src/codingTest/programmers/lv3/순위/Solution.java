52a1d1a6683fa24d9b01424765f48a0fe6edce60
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-06-11 19:49:31 +0900

    pro_순위

---
+++ src/codingTest/programmers/lv3/순위/Solution.java
@@ -0,0 +1,38 @@
+package codingTest.programmers.lv3.순위;
+
+class Solution {
+    public int solution(int n, int[][] results) {
+        boolean[][] win = new boolean[n+1][n+1];
+        for (int[] r : results) {
+            win[r[0]][r[1]] = true;
+        }
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                if (!win[i][k]) {
+                    continue;
+                }
+                for (int j = 1; j <= n; j++) {
+                    if (win[k][j]) {
+                        win[i][j] = true;
+                    }
+                }
+            }
+        }
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            int cnt = 0;
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (win[i][j] || win[j][i]) {
+                    cnt++;
+                }
+            }
+            if (cnt == n - 1) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
