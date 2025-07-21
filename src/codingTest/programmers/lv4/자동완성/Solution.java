f98c71f2b568ee7f0a72e6b7af6823ff9aafc1e4
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-07-21 22:52:56 +0900

    pro_자동 완성

---
+++ src/codingTest/programmers/lv4/자동완성/Solution.java
@@ -0,0 +1,35 @@
+package codingTest.programmers.lv4.자동완성;
+
+import java.util.*;
+
+class Solution {
+    public long solution(String[] words) {
+        int n = words.length;
+        Arrays.sort(words);
+        long total = 0;
+        for (int i = 0; i < n; i++) {
+            int maxCommon = 0;
+            if (i > 0) {
+                maxCommon = Math.max(maxCommon, commonPrefix(words[i], words[i - 1]));
+            }
+            if (i < n - 1) {
+                maxCommon = Math.max(maxCommon, commonPrefix(words[i], words[i + 1]));
+            }
+            total += Math.min(words[i].length(), maxCommon + 1);
+        }
+        return total;
+    }
+
+    private int commonPrefix(String a, String b) {
+        int len = Math.min(a.length(), b.length());
+        int cnt = 0;
+        for (int i = 0; i < len; i++) {
+            if (a.charAt(i) == b.charAt(i)) {
+                cnt++;
+            } else {
+                break;
+            }
+        }
+        return cnt;
+    }
+}
