706b5e7f953944c6c2b02ce86e0fcabf64bf1eb6
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-08-08 22:58:45 +0900

    pro_

---
+++ src/ktb_ct/week03/week02/ct03/Main.java
@@ -0,0 +1,65 @@
+package ktb_ct.week03.week02.ct03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+// 알고리즘 분류 : 이분 탐색, 매개 변수 탐색
+public class Main {
+
+      public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            int[] expenses = new int[N];
+            int maxExpense = 0;
+            int totalExpense = 0;
+
+            for (int i = 0; i < N; i++) {
+                  expenses[i] = Integer.parseInt(br.readLine());
+                  maxExpense = Math.max(maxExpense, expenses[i]);
+                  totalExpense += expenses[i];
+            }
+
+            int left = maxExpense;
+            int right = totalExpense;
+            int result = totalExpense;
+
+            while (left <= right) {
+                  int mid = (left + right) / 2;
+                  if (canWithdrawInMTries(expenses, N, M, mid)) {
+                        result = mid;
+                        right = mid - 1;
+                  } else {
+                        left = mid + 1;
+                  }
+            }
+
+            System.out.println(result);
+
+      }
+
+      private static boolean canWithdrawInMTries(int[] expenses, int N, int M, int K) {
+            int count = 1;
+            int currentSum = 0;
+
+            for (int i = 0; i < N; i++) {
+                  if (currentSum + expenses[i] > K) {
+                        count++;
+                        currentSum = expenses[i];
+                        if (count > M) {
+                              return false;
+                        }
+                  } else {
+                        currentSum += expenses[i];
+                  }
+            }
+
+            return true;
+      }
+}
---
+++ src/ktb_ct/week03/week02/week01/ct04/Main.java
@@ -0,0 +1,57 @@
+package ktb_ct.week03.week02.week01.ct04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 알고리즘 분류 - 누적 합, 슬라이딩 윈도우
+public class Main {
+
+      public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int N = Integer.parseInt(st.nextToken());
+            int X = Integer.parseInt(st.nextToken());
+
+            int[] visitors = new int[N];
+            st = new StringTokenizer(br.readLine());
+
+            for (int i = 0; i < N; i++) {
+                  // 방문자 수 입력
+                  visitors[i] = Integer.parseInt(st.nextToken());
+            }
+
+            int maxVisitors = 0;
+            int currentSum = 0;
+            int count = 0;
+
+            for (int i = 0; i < X; i++) {
+                  currentSum += visitors[i];
+            }
+
+            maxVisitors = currentSum;
+            count = 1;
+
+            // 최대 방문자 수 계산
+            for (int i = X; i < N; i++) {
+                  currentSum = currentSum - visitors[i - X] + visitors[i];
+                  if (currentSum > maxVisitors) {
+                        maxVisitors = currentSum;
+                        count = 1;
+                  } else if (currentSum == maxVisitors) {
+                        count++;
+                  }
+            }
+            if (maxVisitors == 0) {
+                  System.out.println("SAD");
+            } else {
+                  System.out.println(maxVisitors);
+                  System.out.println(count);
+            }
+
+
+      }
+
+}
