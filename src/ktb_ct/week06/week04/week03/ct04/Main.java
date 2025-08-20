feadccd9af826581e721b223836f868f0a9d79ee
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-08-20 23:26:01 +0900

    pro_

---
+++ src/ktb_ct/week06/week04/week03/ct04/Main.java
@@ -0,0 +1,43 @@
+package ktb_ct.week06.week04.week03.ct04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long x = Long.parseLong(st.nextToken());
+        long y = Long.parseLong(st.nextToken());
+
+        long z = (y * 100) / x;
+
+        if (z >= 99) {
+            System.out.println(-1);
+            return;
+        }
+
+        long left = 0;
+        long right = 1000000000;
+        long result = -1;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long newZ = ((y + mid) * 100) / (x + mid);
+
+            if (newZ > z) {
+                result = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        System.out.println(result);
+    }
+
+
+}
