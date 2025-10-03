2572d1bfbc789f5961d2fc090d6d263f5a2de466
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-10-03 22:45:18 +0900

    pro_

---
+++ src/ktb_ct/week01/ct01/올바른_괄호_갯수/Solution.java
@@ -0,0 +1,16 @@
+package ktb_ct.week01.ct01.올바른_괄호_갯수;
+
+class Solution {
+    public int solution(int n) {
+        long[] dp = new long[n + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            long sum = 0;
+            for (int j = 0; j < i; j++) {
+                sum += dp[j] * dp[i - 1 - j];
+            }
+            dp[i] = sum;
+        }
+        return (int) dp[n];
+    }
+}
