b66354490c572af93b921ec3d36c52c56ce4ed81
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2025-11-04 23:16:50 +0900

    pro_

---
+++ src/ktb_ct/week05/ct01/쿠키_구입/Solution.java
@@ -0,0 +1,36 @@
+package ktb_ct.week05.ct01.쿠키_구입;
+
+class Solution {
+    public int solution(int[] cookie) {
+        int n = cookie.length;
+        int answer = 0;
+        for (int m = 0; m < n - 1; m++) {
+            int i = m, j = m + 1;
+            int leftSum = cookie[i], rightSum = cookie[j];
+            while (i >= 0 && j < n) {
+                if (leftSum == rightSum) {
+                    answer = Math.max(answer, leftSum);
+                    i--;
+                    if (i >= 0) {
+                        leftSum += cookie[i];
+                    }
+                    j++;
+                    if (j < n) {
+                        rightSum += cookie[j];
+                    }
+                } else if (leftSum < rightSum) {
+                    i--;
+                    if (i >= 0) {
+                        leftSum += cookie[i];
+                    }
+                } else {
+                    j++;
+                    if (j < n) {
+                        rightSum += cookie[j];
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
